f9be43d6ca18d3485c95c5b92b9d7c042a63e4c5
Stephan Rabanser <steverab93@gmail.com>
2019-09-11 01:05:17 +0200

    Update shared utils

--- shared_utils.py
+++ shared_utils.py
@@ -1,10 +1,12 @@
-import keras
-import pickle
-from keras import backend as K
+# -------------------------------------------------
+# IMPORTS
+# -------------------------------------------------
+
 from enum import Enum
-import types
-import tempfile
-import keras.models
+
+# -------------------------------------------------
+# ENUMS
+# -------------------------------------------------
 
 class TestDimensionality(Enum):
     One = 1
@@ -14,6 +16,7 @@ class TestDimensionality(Enum):
 
 class OnedimensionalTest(Enum):
     KS = 1
+    AD = 2
 
 
 class MultidimensionalTest(Enum):
@@ -32,27 +35,3 @@ class DimensionalityReduction(Enum):
     BBSDs = 5
     BBSDh = 6
     Classif = 7
-
-
-def make_keras_picklable():
-    def __getstate__(self):
-        model_str = ""
-        with tempfile.NamedTemporaryFile(suffix='.hdf5', delete=True) as fd:
-            keras.models.save_model(self, fd.name, overwrite=True)
-            model_str = fd.read()
-        d = { 'model_str': model_str }
-        return d
-
-    def __setstate__(self, state):
-        with tempfile.NamedTemporaryFile(suffix='.hdf5', delete=True) as fd:
-            fd.write(state['model_str'])
-            fd.flush()
-            model = keras.models.load_model(fd.name)
-        self.__dict__ = model.__dict__
-
-
-    cls = keras.models.Model
-    cls.__getstate__ = __getstate__
-    cls.__setstate__ = __setstate__
-
-
